f9f77f1e95c022aca5c1741dddb5272a96657fe6
jackren0000 <91411340+jackren0000@users.noreply.github.com>
2021-10-10 20:01:28 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,8 @@
-var vg_1 = "Homework";
+var vg_1 = "Homework.json";
+var vg_2 = "Homework2.json";
 vegaEmbed("#map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+vegaEmbed("#areachart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
