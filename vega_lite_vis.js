bf34c65536c894521fb5461fca5ffeacc32b577b
jackren0000 <91411340+jackren0000@users.noreply.github.com>
2021-10-19 16:43:48 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,8 +1,24 @@
-var vg_1 = "Homework.json";
-var vg_2 = "Homework2.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+var vg_1 = "GDP.json";
+var vg_2 = "militarymap.json";
+var vg_3 = "happinessdotplot.json";
+var vg_4 = "piechart.json";
+var vg_5 = "GPI.json";
+var vg_6 = "linechart.json";
+vegaEmbed("#vis1", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed("#areachart", vg_2).then(function(result) {
+vegaEmbed("#vis2", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
+vegaEmbed("#vis3", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
+vegaEmbed("#vis4", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
+vegaEmbed("#vis5", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
+vegaEmbed("#vis6", vg_6).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
